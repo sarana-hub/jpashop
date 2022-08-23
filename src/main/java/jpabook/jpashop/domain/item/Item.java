752d1507e6e8207b6cf95ab15b893f41024eb28f
nayeon-jo <nyc0516@naver.com>
2022-08-23 21:03:33 +0900

    상품 엔티티 -비즈니스 로직 추가

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,4 +27,19 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<Category>();
+
+
+    //==비즈니스 로직==//
+    public void addStock(int quantity) {    //stock 증가
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {     //stock 감소
+       int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
+
 }
