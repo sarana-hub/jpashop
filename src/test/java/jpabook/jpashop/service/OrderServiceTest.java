6509be8bc202279af3f0d4143e78e3799ef2776e
nayeon-jo <nyc0516@naver.com>
2022-08-24 16:24:43 +0900

    주문 기능 테스트

--- src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -34,17 +34,18 @@ public class OrderServiceTest {
 
     @Test
     public void 상품주문() throws Exception {
-        //Given
+        //Given     //회원과 상품 생성
         Member member = createMember();
         Item item = createBook("시골 JPA", 10000, 10); //이름, 가격, 재고
         int orderCount = 2;
 
-        //When
+        //When      //실제 상품을 주문
         Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
 
         //Then
         Order getOrder=orderRepository.findOne(orderId);
 
+        //주문 가격이 올바른지, 주문 후 재고 수량이 정확히 줄었는지 검증
         assertEquals("상품 주문시 상태는 ORDER", OrderStatus.ORDER, getOrder.getStatus());
         assertEquals("주문한 상품 종류 수가 정확해야 한다.",1, getOrder.getOrderItems().size());
         assertEquals("주문 가격은 가격 * 수량이다.", 10000 * 2, getOrder.getTotalPrice());
@@ -75,24 +76,26 @@ public class OrderServiceTest {
         Item item = createBook("시골 JPA", 10000, 10); //이름, 가격, 재고
         int orderCount = 2;
 
-        Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
+        Long orderId = orderService.order(member.getId(), item.getId(), orderCount);    //주문
+
         //When
-        orderService.cancelOrder(orderId);
-        //Then
+        orderService.cancelOrder(orderId);  //주문 취소
+
+        //Then  //주문상태가 주문 취소 상태인지(CANCEL), 취소한 만큼 재고가 증가했는지 검증
         Order getOrder = orderRepository.findOne(orderId);
         assertEquals("주문 취소시 상태는 CANCEL 이다.",OrderStatus.CANCEL, getOrder.getStatus());
         assertEquals("주문이 취소된 상품은 그만큼 재고가 증가해야 한다.", 10, item.getStockQuantity());
     }
 
 
-    @Test(expected = NotEnoughStockException.class)
+    @Test(expected = NotEnoughStockException.class)     //NotEnoughStockException 예외가 발생해야함
     public void 상품주문_재고수량초과() throws Exception {
         //Given
         Member member = createMember();
         Item item = createBook("시골 JPA", 10000, 10); //이름, 가격, 재고
-        int orderCount = 11; //재고보다 많은 수량
+        int orderCount = 11; //재고(10)보다 많은 수량
 
-        //When
+        //When  //주문 초과로 예외 발생
         orderService.order(member.getId(), item.getId(), orderCount);
 
         //Then
