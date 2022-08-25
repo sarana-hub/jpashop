a636b3ad02827c1cdb647c66dce8b039b70017a9
nayeon-jo <nyc0516@naver.com>
2022-08-25 12:42:19 +0900

    회원 목록 조회

--- src/main/java/jpabook/jpashop/web/MemberForm.java
+++ src/main/java/jpabook/jpashop/web/MemberForm.java
@@ -5,8 +5,11 @@ import lombok.Setter;
 
 import javax.validation.constraints.NotEmpty;
 
-/**회원 등록 폼 객체
+/**회원 등록 폼 객체*/
+/*
  * 화면 계층과 서비스 계층을 명확하게 분리
+ * 실무에서 엔티티는 핵심 비즈니스 로직만 가지고 있고, 화면을 위한 로직은 없어야 한다
+ * ->화면이나 API에 맞는 폼 객체나 DTO를 사용
  * */
 
 @Getter @Setter
