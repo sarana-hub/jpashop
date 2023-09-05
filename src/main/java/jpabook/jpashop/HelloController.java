0547a3af9618ef410bccc50edbc03f2d2a2351c5
nayeon-jo <nyc0516@naver.com>
2023-09-05 20:50:12 +0900

    View 환경 설정

--- src/main/java/jpabook/jpashop/HelloController.java
+++ src/main/java/jpabook/jpashop/HelloController.java
@@ -4,14 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class HelloController {
 
     @GetMapping("hello")
     public String hello(Model model){
-        model.addAttribute("data", "hello!");
-        return "hello";
+        model.addAttribute("data", "hello~");
+        return "hello";     // templates/hello.html
     }
-    //resources:templates/ +{ViewName}+ .html
-    //resources/templates/hello.html
+
 }
